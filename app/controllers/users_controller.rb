135971a665a3a61fc997428d8a8071f918b2fcaa
wonggamto <wonggamto@github.com>
2022-07-01 00:03:55 +0800

    创建了 model 和 路由

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def show
+    p '你访问了show'
+  end
+
+  def create
+    p '你访问了create'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/users/:id', to: 'users#show'
+  post '/users/', to: 'users#create'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
