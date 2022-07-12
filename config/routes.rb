c3be7fdc6a6d6009494c9646c4cc1c52946b4724
wonggamto <wonggamto@github.com>
2022-07-12 23:39:22 +0800

    创建ValidationCodesController

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # /api/v1
-      resources :validation_codes
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create,:destory]
+      resource :me, only: [:show]
+      resources :items
+      resources :tags 
     end
   end
 end
