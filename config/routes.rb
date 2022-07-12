4a70c0527b3156669b2628daa67e7f211c03ae44
wonggamto <wonggamto@github.com>
2022-07-12 23:20:34 +0800

    创建路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/users/:id', to: 'users#show'
-  post '/users/', to: 'users#create'
-
+  namespace :api do
+    namespace :v1 do
+      # /api/v1
+      resources :validation_codes
+    end
+  end
 end
